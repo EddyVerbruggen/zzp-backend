ec53ebbb1e107b16f7e5a281f87da1f7f2826417
EddyVerbruggen <eddyverbruggen@gmail.com>
2012-09-27 23:50:59 +0200

    Implemented cewl networkupdate filter

--- app/controllers/NetworkController.java
+++ app/controllers/NetworkController.java
@@ -14,9 +14,9 @@ public class NetworkController extends Controller {
 
   public static void updates() {
     NetworkUpdates networkUpdates = new NetworkUpdates();
-    NetworkUpdateType nutFavo = new NetworkUpdateType("img/favourite.png");
-    NetworkUpdateType nutCompany = new NetworkUpdateType("img/company.gif");
-    NetworkUpdateType nutLinkedIn = new NetworkUpdateType("img/linkedin.gif");
+    NetworkUpdateType nutFavo = new NetworkUpdateType("favourite", "img/favourite.png");
+    NetworkUpdateType nutCompany = new NetworkUpdateType("company", "img/company.gif");
+    NetworkUpdateType nutLinkedIn = new NetworkUpdateType("linkedin", "img/linkedin.gif");
 
     // TODO uuid, etc
     final User u1 = new User("u1", new Date(), "img/pasfoto/1.gif", null);
@@ -26,12 +26,12 @@ public class NetworkController extends Controller {
 
     networkUpdates.networkUpdates.add(new NetworkUpdate(u4, Arrays.asList(nutLinkedIn), "22-09-2012", "Ik heb een klus gevonden! Thx all!", 2));
     networkUpdates.networkUpdates.add(new NetworkUpdate(u2, Arrays.asList(nutFavo, nutCompany), "18-09-2012", "Ik hou van deze killer app :)", 1));
-    networkUpdates.networkUpdates.add(new NetworkUpdate(u1, null, "08-09-2012", "Wie weet iets van Magento?", 45));
+    networkUpdates.networkUpdates.add(new NetworkUpdate(u1, Arrays.asList(nutFavo), "08-09-2012", "Wie weet iets van Magento?", 45));
 
     networkUpdates.networkUpdates.add(new NetworkUpdate(u4, Arrays.asList(nutLinkedIn), "01-09-2012", "Ik zoek een PHP opdracht in Amsterdam", 0));
     networkUpdates.networkUpdates.add(new NetworkUpdate(u3, null, "25-08-2012", "Ik zoek een Java klus in de buurt van Enschede", 9));
     networkUpdates.networkUpdates.add(new NetworkUpdate(u4, Arrays.asList(nutLinkedIn), "17-08-2012", "Skills toegevoegd: PHP, HTML, CSS.", 9));
-    networkUpdates.networkUpdates.add(new NetworkUpdate(u1, null, "17-08-2012", "Leuke app dit, eens kijken of ik er wat mee kan.", 3));
+    networkUpdates.networkUpdates.add(new NetworkUpdate(u1, Arrays.asList(nutFavo), "17-08-2012", "Leuke app dit, eens kijken of ik er wat mee kan.", 3));
     networkUpdates.networkUpdates.add(new NetworkUpdate(u2, Arrays.asList(nutFavo, nutCompany), "14-08-2012", "Skills toegevoegd: Java, Scala.", 13));
     networkUpdates.networkUpdates.add(new NetworkUpdate(u4, Arrays.asList(nutLinkedIn), "09-08-2012", "Skills toegevoegd: Scala.", 4));
     networkUpdates.networkUpdates.add(new NetworkUpdate(u4, Arrays.asList(nutLinkedIn), "07-08-2012", "Erm, komen er wel eens Scala klussen langs?", 4));
--- app/models/NetworkUpdateType.java
+++ app/models/NetworkUpdateType.java
@@ -2,8 +2,10 @@ package models;
 
 public class NetworkUpdateType {
   public String name;
+  public String image;
 
-  public NetworkUpdateType(String name) {
+  public NetworkUpdateType(String name, String image) {
     this.name = name;
+    this.image = image;
   }
 }
